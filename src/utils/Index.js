7bf6360e3010fb17ebf30e5b608b81f0f0759703
jdanthdavis <87504521+jdanthdavis@users.noreply.github.com>
2024-11-21 12:59:14 -0500

    Build error fix

--- src/utils/Index.js
+++ src/utils/Index.js
@@ -1,8 +1,8 @@
-export { default as checkKc } from './checkKc.js';
-export { default as grumblerCheck } from './grumblerCheck.js';
-export { default as sanitizedTime } from './sanitizedTime.js';
-export { default as totalLevelCheck } from './totalLevelCheck.js';
-export { default as collectionLogCheck } from './collectionLogCheck.js';
-export { default as petCheck } from './petCheck.js';
-export { default as checkCAProgress } from './checkCAProgress.js';
-export { default as formatPercentage } from './formatPercentage.js';
+export { default as checkKc } from './checkKc';
+export { default as grumblerCheck } from './grumblerCheck';
+export { default as sanitizedTime } from './sanitizedTime';
+export { default as totalLevelCheck } from './totalLevelCheck';
+export { default as collectionLogCheck } from './collectionLogCheck';
+export { default as petCheck } from './petCheck';
+export { default as checkCAProgress } from './checkCAProgress';
+export { default as formatPercentage } from './formatPercentage';

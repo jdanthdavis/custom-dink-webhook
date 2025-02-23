d0ca5de3535752ab0ac2c5eff079913136f216c0
jdanthdavis <87504521+jdanthdavis@users.noreply.github.com>
2025-02-23 16:32:11 -0500

    Format drops

---
+++ src/utils/formatPrice/formatPrice.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * @param {*} value
+ * @returns
+ */
+function formatPrice(value) {
+  if (value >= 2_147_000_000) return '**Very valuable**';
+  if (value >= 1_000_000_000)
+    return `**${(value / 1_000_000_000).toFixed(2).replace(/\.00$/, '')}B**`;
+  if (value >= 1_000_000)
+    return `**${(value / 1_000_000).toFixed(2).replace(/\.00$/, '')}M**`;
+  if (value >= 1_000)
+    return `**${(value / 1_000).toFixed(1).replace(/\.0$/, '')}K**`;
+  return `**${value}**`;
+}
+
+export default formatPrice;

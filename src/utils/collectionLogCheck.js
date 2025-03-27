84d5bea83a06e17c229b4a2e53af34ff6eeb2325
Justin <jd.anthdavis@gmail.com>
2025-03-27 13:45:36 -0400

    Add clog rank to message.

--- src/utils/collectionLogCheck.js
+++ src/utils/collectionLogCheck.js
@@ -10,11 +10,22 @@ import formatAsPercentage from "./formatAsPercentage.js";
  * @returns
  */
 function collectionLogCheck(msgMap, playerName, extra, URL) {
-  const { totalEntries, completedEntries, clogRank } = extra || {};
+  const { totalEntries, completedEntries, currentRank } = extra || {};
   const itemName = grumblerCheck(extra?.itemName);
   const percentageCompleted = formatAsPercentage(
     (completedEntries / totalEntries) * 100
   );
+  const rankMap = {
+    BRONZE: "<:bronze_rank:1353119905750192209>",
+    IRON: "<:iron_rank:1353119911643320390>",
+    STEEL: "<:steel_rank:1353119914403172363>",
+    BLACK: "<:black_rank:1353119904550883409>",
+    MITHRIL: "<:mithril_rank:1352512785191538708>",
+    ADAMANT: "<:adamant_rank:1353119902403137658>",
+    RUNE: "<:rune_rank:1353119913115521228>",
+    DRAGON: "<:dragon_rank:1353119907864121425>",
+    GILDED: "<:gilded_rank:1353119909844095016>",
+  };
 
   if (!totalEntries || !completedEntries) {
     // If the user hasn't cycled their collection log we will use this fallback to prevent errors
@@ -26,7 +37,7 @@ function collectionLogCheck(msgMap, playerName, extra, URL) {
   } else {
     msgMap.set(
       { ID: "COLLECTION_LOG", URL: URL },
-      `**${playerName}** has added a new item to their collection log: **${itemName}** | **${completedEntries}/${totalEntries} (${percentageCompleted}%)** | ${test}`
+      `**${playerName}** has added a new item to their collection log: **${itemName}** | **${completedEntries}/${totalEntries} (${percentageCompleted}%)** | ${rankMap[currentRank]}`
     );
   }
   return msgMap;

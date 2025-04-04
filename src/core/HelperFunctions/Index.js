20e433fd358fb62ea34a951085ad26d0ba92f814
Swap <87504521+jdanthdavis@users.noreply.github.com>
2025-04-04 14:17:24 -0400

    Delete src directory

--- src/core/HelperFunctions/Index.js
+++
@@ -1,4 +0,0 @@
-export { default as GrumblerCheck } from './GrumblerCheck.js';
-export { default as KillCountMsgConstructor } from './KillCountMsgConstructor.js';
-export { default as FormatAsPercentage } from './FormatAsPercentage.js';
-export { default as FormatPrice } from './FormatPrice.js';

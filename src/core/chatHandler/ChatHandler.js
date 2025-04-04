543bedd0f0f2252bce6adc34e400b9c421ac895e
Justin <jd.anthdavis@gmail.com>
2025-04-04 14:00:23 -0400

    Grab case changes

---
+++ src/core/chatHandler/ChatHandler.js
@@ -0,0 +1,34 @@
+import * as Constants from './Constants';
+import { BigFishHandler } from './BigFishHandler';
+import { SepulchreHandler } from './SepulchreHandler';
+import { VestigeHandler } from './VestigeHandler';
+
+/**
+ * Handles various chat messages.
+ * @param {Map<{ ID: string, URL: string}, string>} msgMap - The message map to update
+ * @param {*} playerName - The player's name
+ * @param {*} extra - Additional information. See {@link https://github.com/pajlads/DinkPlugin/blob/master/docs/json-examples.md#chat} for all the information.
+ * @param {*} typeOfChat - The chat type. See {@link https://github.com/pajlads/DinkPlugin/blob/master/docs/json-examples.md#chat} for the list of possible chat types.
+ * @param {*} URL - The associated URL
+ */
+function ChatHandler(msgMap, playerName, extra, typeOfChat, URL) {
+  const message = extra?.message;
+  const chatType = Constants.CHAT_MESSAGE_TYPES;
+
+  switch (typeOfChat) {
+    case chatType.BIG_FISH:
+      BigFishHandler(msgMap, playerName, message, URL);
+      break;
+    case chatType.VESTIGE_DROP:
+      VestigeHandler(msgMap, playerName, msgmessageMap, URL);
+      break;
+    case chatType.NEW_PERSONAL_BEST:
+      SepulchreHandler(msgMap, playerName, message, URL);
+      break;
+    default:
+      console.log(`Unknown type of chat: ${typeOfChat}`);
+      break;
+  }
+}
+
+export default ChatHandler;
---
+++ src/core/chatHandler/VestigeHandler.js
@@ -0,0 +1,23 @@
+import * as Constants from './Constants';
+
+/**
+ * Handles when a player gets an untradable vestige drop.
+ * @param {Map<{ ID: string, URL: string}, string>} msgMap - The message map to update
+ * @param {string} playerName - The player's name
+ * @param {string} message - The in-game message
+ * @param {string} URL - The associated URL
+ * @returns {Map<{ ID: string, URL: string }, string>} The updated message map
+ */
+export function VestigeHandler(msgMap, playerName, message, URL) {
+  const match = message?.match(Constants.CHAT_REGEX.VESTIGE_TEXT);
+
+  if (!match) return;
+
+  const vestigeType = match[1];
+  const bossName = Constants.VESTIGE_MAP[vestigeType];
+  const msg = `**${playerName}** has received **x1 ${vestigeType} vestige (5M)** from **${bossName}!**`;
+
+  msgMap.set({ ID: Constants.CHAT_MESSAGE_TYPES.VESTIGE_DROP, URL }, msg);
+
+  return msgMap;
+}

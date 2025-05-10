277fd13ffbdc1d6c209966cdad8d720671822097
Swap <jd.anthdavis@gmail.com>
2025-05-10 00:30:32 -0400

    kitMatch[1] not [2]

--- src/core/chatMsgHandler/tobKitHandler.js
+++ src/core/chatMsgHandler/tobKitHandler.js
@@ -8,7 +8,7 @@ export function tobKitHandler(message, playerName, msgMap, URL) {
   const kitMatch = message.match(CHAT_REGEX.TOB_KITS);
   if (!kitMatch) return;
 
-  const item = kitMatch[2];
+  const item = kitMatch[1];
   const msg = `**${playerName}** has received **${item}** from **${THEATRE_OF_BLOOD_HM}!**`;
 
   msgMap.set({ ID: CHAT_MESSAGE_TYPES.TOB_KIT, URL }, msg);

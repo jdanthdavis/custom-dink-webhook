671f2197e50082984be2ec962e23f4d130a8af45
Swap <87504521+jdanthdavis@users.noreply.github.com>
2025-05-15 13:51:12 -0400

    Add ‘success’ to kc constructor

--- src/core/helperFunctions/killCountMsgConstructor.js
+++ src/core/helperFunctions/killCountMsgConstructor.js
@@ -7,7 +7,7 @@
  */
 function killCountMsgConstructor(playerName, gameMessage, bossName, killCount) {
   const primary_regex =
-    /Your (?<key>.+)\s(?<type>kill|chest|completion|harvest)\s?count is: [\d,]+\b/i;
+    /Your (?<key>.+)\s(?<type>kill|chest|completion|success|harvest)\s?count is: [\d,]+\b/i;
   const secondary_regex =
     /Your (?<type>completed|subdued) (?<key>.+?) count is: [\d,]+\b/i;
 

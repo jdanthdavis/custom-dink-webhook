0da6e82c135945c38c2844b04e48820914a13be5
Swap <jd.anthdavis@gmail.com>
2025-05-30 14:48:40 -0400

    Add missing UNTRADEABLE_TEXT regex

--- src/core/chatMsgHandler/untradeableDropHandler.js
+++ src/core/chatMsgHandler/untradeableDropHandler.js
@@ -4,7 +4,7 @@ export function untradeableDropHandler(message, playerName, msgMap, URL) {
   //some code for cloudflare to find
   let msg;
   const vestigeMatch = message?.match(CHAT_REGEX.VESTIGE_TEXT);
-  const untradeableMatch = message?.match(CHAT_REGEX.UNTRADEABLE_DROP);
+  const untradeableMatch = message?.match(CHAT_REGEX.UNTRADEABLE_TEXT);
   const tobKitMatch = message?.match(CHAT_REGEX.TOB_KITS);
   if (!vestigeMatch && !untradeableMatch && !tobKitMatch) return;
 

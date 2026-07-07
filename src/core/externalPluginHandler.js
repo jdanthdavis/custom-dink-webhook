afc3ff345323b5ed5f869f8698b36f7cb84eeb8e
Swap <jd.anthdavis@gmail.com>
2026-07-06 22:15:58 -0400

    Initial externalPlugin logic

--- src/core/externalPluginHandler.js
+++ src/core/externalPluginHandler.js
@@ -1,5 +1,10 @@
-function externalPluginHandler(msgMap, items, playerName, source, URL) {
-  console.log('externalPluginHandler');
+import { EXTERNAL_PLUGIN } from "../constants";
+
+function externalPluginHandler(msgMap, playerName, content, metaData, URL) {
+  const msg = `**${playerName}** has received ${metaData.cardName}!**`;
+  msgMap.set({ ID: EXTERNAL_PLUGIN, URL }, msg);
+
+  return msgMap;
 }
 
 export default externalPluginHandler;
